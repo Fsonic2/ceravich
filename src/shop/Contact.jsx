b952e64460116ef097de2a9925203e7eb37305da
fsonic2 <fsonic3@gmail.com>
2026-03-10 17:15:40 +0100

    added google map tradefair lagos

--- src/shop/Contact.jsx
+++ src/shop/Contact.jsx
@@ -40,6 +40,16 @@ export default function Contact() {
         </div>
       </div>
     </div>
+    <div className="container py-5">
+        google map here
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.4820970765104!2d3.244035873768495!3d6.460440023895487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b866caf1ccdcb%3A0xf26aea9ac338c46e!2sTradefairlagos!5e0!3m2!1sen!2sus!4v1773159238355!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+    </div>
     <Footer />
     </>
   );
