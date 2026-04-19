b489fd4d357a12b70121f0a5f61660d5025fc16a
akinzsoft <85848497+akinzsoft@users.noreply.github.com>
2026-04-19 21:25:02 +0100

    Delete src directory

--- src/shop/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="container text-center py-5">
-      <h1 className="display-4">404</h1>
-      <p className="lead">Page not found</p>
-      <a href="/" className="btn btn-info">Go Home</a>
-    </div>
-  );
-}
